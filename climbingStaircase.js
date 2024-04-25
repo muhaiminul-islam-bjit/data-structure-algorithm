9a651f9ce707011ad26dda8809e9a2a9c6e54539
muhaiminul-islam-bjit <muhaiminul.islam@bjitgroup.com>
2024-04-25 13:21:06 +0600

    implemented climbing staircases

---
+++ climbingStaircase.js
@@ -0,0 +1,11 @@
+function stairCase(n) {
+  const numberOfWays = [1, 2];
+
+  for (let i = 2; i < n; i++) {
+    numberOfWays[i] = numberOfWays[i - 1] + numberOfWays[i - 2];
+  }
+
+  return numberOfWays[n - 1];
+}
+
+console.log(stairCase(6))
